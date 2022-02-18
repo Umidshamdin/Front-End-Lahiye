5404480862c668da79f1d72742ccacdad0df8985
[umid shamdinli] <[umidash@code.edu.az]>
2022-02-18 18:23:22 +0400

    Contact page added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,20 @@
-// $(document).ready(function(){
-//     $('.dropdown-submenu a.test').on("click", function(e){
-//       $(this).next('ul').toggle();
-//       e.stopPropagation();
-//       e.preventDefault();
-//     });
-//   });
-//   // Set the date we're counting down to
-// var countDownDate = new Date("Mart 5, 2022 15:37:25").getTime();
-
-// // Update the count down every 1 second
-// var x = setInterval(function() {
-
-//   // Get today's date and time
-//   var now = new Date().getTime();
-    
-//   // Find the distance between now and the count down date
-//   var distance = countDownDate - now;
-    
-//   // Time calculations for days, hours, minutes and seconds
-//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-//   // Output the result in an element with id="demo"
-//   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-//   + minutes + "m " + seconds + "s ";
-    
-//   // If the count down is over, write some text 
-//   if (distance < 0) {
-//     clearInterval(x);
-//     document.getElementById("demo").innerHTML = "EXPIRED";
-//   }
-// }, 1000);
-
-$(function(){
-
-
-  // function myFunction() {
-  //   var x = document.getElementById("Demo");
-  //   if (x.className.indexOf("w3-show") == -1) {
-  //     x.className += " w3-show";
-  //   } else { 
-  //     x.className = x.className.replace(" w3-show", "");
-  //   }
-  // }
-
-
-
-
-
-
-
+$(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
 })
+
+
